29a9327ab20515ef6dae5b71ffb672e48e069cd0
Joel Messerli <joel@jmnw.me>
2016-06-28 13:40:08 +0200

    fixed /ping, added skeleton for /connections and started work on /interactive

--- Api/TransportAPI.php
+++ Api/TransportAPI.php
@@ -4,6 +4,7 @@ namespace PegNu\Api;
 
 use GuzzleHttp\Client;
 use PegNu\Api\Model\Location;
+use PegNu\Api\Model\Connection;
 
 class TransportAPI
 {
@@ -36,23 +37,31 @@ class TransportAPI
 
         return $stations;
     }
+
     /**
-     * @param Location $from 
-     * @param Location $to 
-     * @param string $time 
-     * @param bool $isArrivalTime 
+     * @param Location $from
+     * @param Location $to
+     * @param string $time
+     * @param bool $isArrivalTime
      */
     public function getConnections($from, $to, $time, $isArrivalTime)
     {
         $response = $this->client->request("GET", "connections", [
             "query" => [
-                "from" => $from["id"],
-                "to" => $to["id"],
+                "from" => $from->id,
+                "to" => $to->id,
                 "time" => $time,
                 "isArrivalTime" => $isArrivalTime
             ]
         ]);
 
+        $responseData = json_decode((string)$response->getBody(), true);
+        $connectionsRaw = $responseData["connections"];
+        $connections = [];
 
+        foreach ($connectionsRaw as $connectionRaw)
+            $connections[] = Connection::fromJson($connectionRaw);
+
+        return $connections;
     }
 }
---
+++ SlackHelper.php
@@ -0,0 +1,7 @@
+<?php
+namespace PegNu;
+
+class SlackHelper
+{
+
+}
